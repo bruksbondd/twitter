d769922c18531c7e85d4242ed6184445309ec64c
bruksbondd <bruksbond@gmail.com>
2020-09-25 12:50:12 +0300

    frontent ready

---
+++ src/components/AddTweetForm.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import classNames from 'classnames';
+import Avatar from '@material-ui/core/Avatar';
+import Alert from '@material-ui/lab/Alert';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import IconButton from '@material-ui/core/IconButton';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
+import EmojiIcon from '@material-ui/icons/SentimentSatisfiedOutlined';
+import { useHomeStyles } from '../pages/Home/theme';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAddTweet } from '../store/ducks/tweets/actionCreators';
+import { selectAddFormState } from '../store/ducks/tweets/selectors';
+import { AddFormState } from '../store/ducks/tweets/contracts/state';
+
+interface AddTweetFormProps {
+  classes: ReturnType<typeof useHomeStyles>;
+  maxRows?: number;
+}
+
+const MAX_LENGTH = 280;
+
+export const AddTweetForm: React.FC<AddTweetFormProps> = ({
+  classes,
+  maxRows,
+}: AddTweetFormProps): React.ReactElement => {
+  const dispatch = useDispatch();
+  const [text, setText] = React.useState<string>('');
+
+  const addFormState = useSelector(selectAddFormState);
+  const textLimitPercent = Math.round((text.length / 280) * 100);
+  const textCount = MAX_LENGTH - text.length;
+
+  const handleChangeTextare = (e: React.FormEvent<HTMLTextAreaElement>): void => {
+    if (e.currentTarget) {
+      setText(e.currentTarget.value);
+    }
+  };
+
+  const handleClickAddTweet = (): void => {
+    dispatch(fetchAddTweet(text));
+    setText('');
+  };
+
+  return (
+    <div>
+      <div className={classes.addFormBody}>
+        <Avatar
+          className={classes.tweetAvatar}
+          alt={`Аватарка пользователя UserAvatar`}
+          src="https://pbs.twimg.com/profile_images/796061890451542016/J-O1AguD_bigger.jpg"
+        />
+        <TextareaAutosize
+          onChange={handleChangeTextare}
+          className={classes.addFormTextarea}
+          placeholder="Что происходит?"
+          value={text}
+          rowsMax={maxRows}
+        />
+      </div>
+      <div className={classes.addFormBottom}>
+        <div className={classNames(classes.tweetFooter, classes.addFormBottomActions)}>
+          <IconButton color="primary">
+            <ImageOutlinedIcon style={{ fontSize: 26 }} />
+          </IconButton>
+          <IconButton color="primary">
+            <EmojiIcon style={{ fontSize: 26 }} />
+          </IconButton>
+        </div>
+        <div className={classes.addFormBottomRight}>
+          {text && (
+            <>
+              <span>{textCount}</span>
+              <div className={classes.addFormCircleProgress}>
+                <CircularProgress
+                  variant="static"
+                  size={20}
+                  thickness={5}
+                  value={text.length >= MAX_LENGTH ? 100 : textLimitPercent}
+                  style={text.length >= MAX_LENGTH ? { color: 'red' } : undefined}
+                />
+                <CircularProgress
+                  style={{ color: 'rgba(0, 0, 0, 0.1)' }}
+                  variant="static"
+                  size={20}
+                  thickness={5}
+                  value={100}
+                />
+              </div>
+            </>
+          )}
+          <Button
+            onClick={handleClickAddTweet}
+            disabled={addFormState === AddFormState.LOADING || !text || text.length >= MAX_LENGTH}
+            color="primary"
+            variant="contained">
+            {addFormState === AddFormState.LOADING ? (
+              <CircularProgress color="inherit" size={16} />
+            ) : (
+              'Твитнуть'
+            )}
+          </Button>
+        </div>
+      </div>
+      {addFormState === AddFormState.ERROR && (
+        <Alert severity="error">
+          Ошибка при добавлении твита{' '}
+          <span aria-label="emoji-plak" role="img">
+            😞
+          </span>
+        </Alert>
+      )}
+    </div>
+  );
+};
---
+++ src/pages/Home/components/FullTweet.tsx
@@ -0,0 +1,65 @@
+import Avatar from '@material-ui/core/Avatar';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchTweetData, setTweetData } from '../../../store/ducks/tweet/actionCreators';
+import { selectIsTweetLoading, selectTweetData } from '../../../store/ducks/tweet/selectors';
+import { useHomeStyles } from '../theme';
+
+export const FullTweet: React.FC = (): React.ReactElement | null => {
+  const classes = useHomeStyles();
+  const dispatch = useDispatch();
+  const tweetData = useSelector(selectTweetData);
+  const isLoading = useSelector(selectIsTweetLoading);
+  const params: { id?: string } = useParams();
+  const id = params.id;
+
+  React.useEffect(() => {
+    if (id) {
+      dispatch(fetchTweetData(id));
+    }
+
+    return () => {
+      dispatch(setTweetData(undefined));
+    };
+  }, [dispatch, id]);
+
+  if (isLoading) {
+    return (
+      <div className={classes.tweetsCentred}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (tweetData) {
+    return (
+      <Paper className={classes.fullTweet}>
+        <div className={classNames(classes.tweetsHeaderUser)}>
+          <Avatar
+            className={classes.tweetAvatar}
+            alt={`Аватарка пользователя ${tweetData.user.fullname}`}
+            src={tweetData.user.avatarUrl}
+          />
+          <Typography>
+            <b>{tweetData.user.fullname}</b>&nbsp;
+            <div>
+              <span className={classes.tweetUserName}>@{tweetData.user.username}</span>&nbsp;
+              <span className={classes.tweetUserName}>·</span>&nbsp;
+              <span className={classes.tweetUserName}>1 ч</span>
+            </div>
+          </Typography>
+        </div>
+        <Typography className={classes.fullTweetText} gutterBottom>
+          {tweetData.text}
+        </Typography>
+      </Paper>
+    );
+  }
+
+  return null;
+};
